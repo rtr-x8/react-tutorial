b46a750f155e0cee7e8f49fafd94833bd87e04bd
ryutaromatsuoka <ryu.2.24@hotmail.co.jp>
2019-05-20 20:47:22 +0900

    change status

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next Player: X';
+        const status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
