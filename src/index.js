44d9dd357740d3c1d6685dd4e44f4fd77f4457d9
ryutaromatsuoka <ryu.2.24@hotmail.co.jp>
2019-05-20 20:35:40 +0900

    change state/props from Square to Board

--- src/index.js
+++ src/index.js
@@ -3,22 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
         return (
             <button
                 className="square"
-                onClick={() => this.setState({
-                    value: 'X'
-                })
-            }>
-                {this.state.value}
+                onClick={() => this.props.onClick()}
+            >
+                {this.props.value}
             </button>
         );
     }
@@ -26,8 +17,27 @@ class Square extends React.Component {
 
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+        console.log(this.state.squares)
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
